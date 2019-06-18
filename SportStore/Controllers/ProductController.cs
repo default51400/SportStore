034d0ddaee8415330cafa981ec5a6b6badb57984
Al Pa Ho <petrov51400@gmail.com>
2019-06-19 02:00:39 +0300

    Init proj, add MVC settings, ProductC+V, ListV, add EF to SQL and First migration

---
+++ SportStore/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportStore.Models;
+
+namespace SportStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository _repository;
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ViewResult List() => View(_repository.Products);
+    }
+}
---
+++ SportStore/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SportStore.Models;
+using SportStore.Models.Repository;
+
+
+namespace SportStore
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration) =>
+           Configuration = configuration;
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IProductRepository, FakeProductRepository>();
+            services.AddMvc();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(name: default, template: "{controller=Product}/{action=List}/{id?}");
+            });
+        }
+    }
+}
